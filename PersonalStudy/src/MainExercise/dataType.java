83d0e1d65ecbd0f69541bb87ffd159bfe9a7f79a
Timothy-JH-Park <pgh1173@gmail.com>
2021-06-10 20:57:34 +0900

    Create dataType.java

---
+++ PersonalStudy/src/MainExercise/dataType.java
@@ -0,0 +1,26 @@
+package MainExercise;
+
+public class dataType {
+
+	public static void main(String[] args) {
+		
+//		for (char i = 'a'; i <= 'z'; i++)
+//		{
+//			System.out.print(i + " ");
+//		}
+		
+//		int a = 200;
+//		
+//		System.out.println("10진수 : " + a);
+//		System.out.format("8진수 : %o\n", a);
+//		
+//		System.out.format("16진수 : %x", a);
+		
+		String name = "John Doe";
+		System.out.println(name);
+		System.out.println(name.substring(0, 1));
+		System.out.println(name.substring(3, 6));
+		System.out.println(name.substring(5, 8));
+		
+	}
+}
