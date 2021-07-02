7f9663af042943a2c14e6ed3f4882f20d546ffe0
Timothy-JH-Park <pgh1173@gmail.com>
2021-07-02 23:10:52 +0900

    Create DateClient.java

---
+++ NetworkPrograming/src/server/DateClient.java
@@ -0,0 +1,21 @@
+package server;
+
+import java.net.*;
+import java.io.*;
+
+public class DateClient {
+
+	public static void main(String[] args) {
+		try (Socket socket = new Socket("localhost", 9101)) {
+			
+			InputStream stream = socket.getInputStream();
+			InputStreamReader reader = new InputStreamReader(stream);
+			BufferedReader bufReader = new BufferedReader(reader);
+			String line = bufReader.readLine();
+			System.out.println(line);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
