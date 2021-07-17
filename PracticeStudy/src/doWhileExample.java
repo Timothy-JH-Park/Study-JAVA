7347779f99fe61287b65986dd2728147ac0b74d9
Timothy-JH-Park <pgh1173@gmail.com>
2021-07-17 23:47:11 +0900

    Create doWhileExample.java

---
+++ PracticeStudy/src/doWhileExample.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class doWhileExample {
+		
+	public static void main(String[] args) {
+	  System.out.println("메시지를 입력하세요.");
+	  System.out.println("프로그램을 종료하려면 q를 입력하세요");
+	
+	  Scanner sc = new Scanner(System.in);
+	  String inputString;
+	
+	  do {
+	  System.out.print(">");
+	  inputString = sc.nextLine();
+	  System.out.println(inputString);
+	  
+	  } while (!inputString.contentEquals("q"));
+	  System.out.println();
+	  System.out.println("프로그램 종료");
+	  sc.close();
+	}
+}
