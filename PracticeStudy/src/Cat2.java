834f4f8243e0b506423ac8381050b1324744babb
Timothy-JH-Park <pgh1173@gmail.com>
2021-07-22 20:12:27 +0900

    Create Cat2.java

---
+++ PracticeStudy/src/Cat2.java
@@ -0,0 +1,14 @@
+
+public class Cat2 extends Animal{
+ 
+    public Cat2(){
+        this.kind = "Æ÷À¯·ù";
+    }
+    @Override
+    public void sound() {
+        // TODO Auto-generated method stub
+        System.out.println("¾ß~¿Ë!");
+    }
+ 
+}
+
