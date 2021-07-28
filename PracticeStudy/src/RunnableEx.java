6beae796193a15574ea47d76d040a9d7fbf54223
Timothy-JH-Park <pgh1173@gmail.com>
2021-07-28 23:38:53 +0900

    Create RunnableEx.java

---
+++ PracticeStudy/src/RunnableEx.java
@@ -0,0 +1,13 @@
+package lambda;
+
+public class RunnableEx {
+    public static void main(String[] args) {
+        Runnable runnable = () -> {
+            for (int i = 0; i < 30; i++) {
+                System.out.println(i);
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+}
