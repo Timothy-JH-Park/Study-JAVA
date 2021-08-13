9a6d298cb39d818868da77a19dba4d3f78928ab3
Timothy-JH-Park <pgh1173@gmail.com>
2021-08-13 20:22:32 +0900

    Create charAt.java

---
+++ PracticeStudy/src/charAt.java
@@ -0,0 +1,13 @@
+package Object2;
+
+public class charAt {
+	public static void main(String[] args) {
+		String str = new String("Java");
+		System.out.println("원본 문자열 : " + str);
+		
+		for (int i = 0; i < str.length(); i++) {
+			System.out.print(str.charAt(i) + " ");
+		}
+		System.out.println("\ncharAt() 메소드 호출 후 원본 문자열 : " + str);
+	}
+}
