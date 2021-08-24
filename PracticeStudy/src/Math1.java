bd7474caf6fee6b00ddf11bd534cbb1855a1acd4
Timothy-JH-Park <pgh1173@gmail.com>
2021-08-24 18:11:33 +0900

    Create Math1.java

---
+++ PracticeStudy/src/Math1.java
@@ -0,0 +1,12 @@
+package Object2;
+
+import java.util.*;
+
+public class Math1 {
+	public static void main(String[] args) {
+		System.out.println((int)(Math.random() * 100));
+		
+		Random ran = new Random();
+		System.out.println(ran.nextInt(100));
+	}
+}
