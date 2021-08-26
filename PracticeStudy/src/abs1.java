619d9f3830060c4fdff4ea7d1427af2ffafc5d91
Timothy-JH-Park <pgh1173@gmail.com>
2021-08-26 23:53:53 +0900

    Create abs1.java

---
+++ PracticeStudy/src/abs1.java
@@ -0,0 +1,9 @@
+package Object2;
+
+public class abs1 {
+	public static void main(String[] args) {
+		System.out.println(Math.abs(10));		// 10
+		System.out.println(Math.abs(-10));		// 10
+		System.out.println(Math.abs(-3.14));	// 3.14
+	}
+}
