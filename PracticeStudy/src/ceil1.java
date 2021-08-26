3acc50460d88a466549b6232562bc2c7fb161d11
Timothy-JH-Park <pgh1173@gmail.com>
2021-08-26 23:53:26 +0900

    Create ceil1.java

---
+++ PracticeStudy/src/ceil1.java
@@ -0,0 +1,16 @@
+package Object2;
+
+public class ceil1 {
+	public static void main(String[] args) {
+		System.out.println(Math.ceil(10.0));		// 10.0
+		System.out.println(Math.ceil(10.1));		// 11.0
+		System.out.println(Math.ceil(10.000001));	// 11.0
+		
+		System.out.println(Math.floor(10.0));		// 10.0
+		System.out.println(Math.floor(10.9));		// 10.0
+		
+		System.out.println(Math.round(10.0));		// 10
+		System.out.println(Math.round(10.4));		// 10
+		System.out.println(Math.round(10.5));		// 11
+	}
+}
