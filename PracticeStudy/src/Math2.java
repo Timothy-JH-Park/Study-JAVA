94957cafeb40ba9a43c5fe275bb293db8fee3047
Timothy-JH-Park <pgh1173@gmail.com>
2021-08-30 23:44:06 +0900

    Create Math2.java

---
+++ PracticeStudy/src/Math2.java
@@ -0,0 +1,12 @@
+package Object2;
+
+public class Math2 {
+	public static void main(String[] args) {
+		System.out.println(Math.max(3.14, 3.14159));	// 3.14159
+		System.out.println(Math.min(3.14, 3.14159));	// 3.14
+		System.out.println();
+		
+		System.out.println(Math.max(-10, -11));			// -10
+		System.out.println(Math.min(-10, -11));			// -11
+	}
+}
