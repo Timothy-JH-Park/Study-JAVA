928c07d1aada15ad2f502ab075844f8616880a7d
Timothy-JH-Park <pgh1173@gmail.com>
2021-08-31 23:46:50 +0900

    Create sqrt1.java

---
+++ PracticeStudy/src/sqrt1.java
@@ -0,0 +1,8 @@
+package Object2;
+
+public class sqrt1 {
+	public static void main(String[] args) {
+		System.out.println((int)Math.pow(5, 2));	// 25
+		System.out.println((int)Math.sqrt(25));		// 5
+	}
+}
