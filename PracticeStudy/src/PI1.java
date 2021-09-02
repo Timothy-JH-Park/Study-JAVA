ed1b277da044b14e002a589edb5355dfcb0ec968
Timothy-JH-Park <pgh1173@gmail.com>
2021-09-02 23:58:59 +0900

    Create PI1.java

---
+++ PracticeStudy/src/PI1.java
@@ -0,0 +1,14 @@
+package Object2;
+
+public class PI1 {
+	public static void main(String[] args) {
+		System.out.println(Math.sin(Math.toRadians(30)));
+		System.out.println(Math.sin(Math.PI / 6));
+		
+		System.out.println(Math.tan(Math.toRadians(45)));
+		System.out.println(Math.tan(Math.PI / 4));
+		
+		System.out.println(Math.cos(Math.toRadians(60)));
+		System.out.println(Math.cos(Math.PI / 3));
+	}
+}
